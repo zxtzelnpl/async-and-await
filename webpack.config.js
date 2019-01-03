b55be81156a7f3c74ce8266796c958545e07d8c6
zxt <zxt_zel_npl@163.com>
2019-01-03 09:08:18 +0800

    change the webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ module.exports = {
     devtool: '#source-map',
     devServer: {
         contentBase: './dist',
+        host:'0.0.0.0',
         historyApiFallback: false,
         overlay: true,
+        useLocalIp:true,
     },
 }
